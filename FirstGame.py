f3b0808dd87b1ac615c82943eb66afcd094d5e17
macarDesktop <cardinalemiguel@gmail.com>
2021-10-25 16:54:55 -0700

    Class Player Sprite

--- FirstGame.py
+++ FirstGame.py
@@ -50,6 +50,39 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 running = True
 
 
+# Fill the screen with white
+
+screen.fill((255, 255, 255))
+
+
+# Create a surface and pass in a tuple containing its length and width
+
+surf = pygame.Surface((50, 50))
+
+
+# Give the surface a color to separate it from the background
+
+surf.fill((0, 0, 0))
+
+rect = surf.get_rect()
+
+# Put the center of surf at the center of the display
+
+surf_center = (
+
+    (SCREEN_WIDTH-surf.get_width())/2,
+
+    (SCREEN_HEIGHT-surf.get_height())/2
+
+)
+
+
+# Draw surf at the new coordinates
+
+screen.blit(surf, surf_center)
+
+pygame.display.flip()
+
 # Main loop
 
 while running:
