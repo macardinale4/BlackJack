adcd4678db42ac490e719b88eb45672dac0dd2cb
[Mike's Desktop] <cardinalemiguel@gmail.com>
2021-11-06 18:04:17 -0700

    Fixed broken code with skills learned from other tutorial

--- FirstGame.py
+++ FirstGame.py
@@ -24,11 +24,12 @@ class Player(pygame.sprite.Sprite):
     def __init__(self):
         super(Player, self).__init__()
         self.surf = pygame.Surface((75, 25))
-        self.surf.fill((255, 255, 255))
+        self.surf.fill((255, 255, 0))
         self.rect = self.surf.get_rect()
 
 # Move the sprite based on user keypresses
-def update(self, pressed_keys):
+def updated(self, pressed_keys):
+    #print("YO")
     if pressed_keys[K_UP]:
         self.rect.move_ip(0, -5)
     if pressed_keys[K_DOWN]:
@@ -56,25 +57,31 @@ while running:
     # for loop through the event queue
     for event in pygame.event.get():
         # Check for KEYDOWN event
-        if event.type == KEYDOWN:
-            # If the Esc key is pressed, then exit the main loop
-            if event.key == K_ESCAPE:
-                running = False
-        # Check for QUIT event. If QUIT, then set running to false.
-        elif event.type == QUIT:
+        #if event.key == K_ESCAPE:
+                #running = False
+        if event.type == QUIT:
             running = False
-    # Get all the keys currently pressed
-    #pressed_keys = pygame.key.get_pressed()
-    pressed_keys = [K_DOWN]
-
-    # Update the player sprite based on user keypresses
-    player.update(pressed_keys)
-
-    # Fill the screen with black
+        # Check for QUIT event. If QUIT, then set running to false.
+        
+        # Get all the keys currently pressed
+        # Fill the screen with black
     screen.fill((0, 0, 0))
 
-    # Draw the player on the screen
+        # Draw the player on the screen
     screen.blit(player.surf, player.rect)
 
-    # Update the display
-    pygame.display.flip()
+    pressed_keys = pygame.key.get_pressed()
+    #if pressed_keys[pygame.K_LEFT]: #LEFT ARROW
+        #print("sugma") 
+    updated(player, pressed_keys)
+    
+    
+        #pressed_keys = pygame.key.get_pressed()
+        #pressed_keys = [K_DOWN]
+
+        # Update the player sprite based on user keypresses
+        #player.update(pressed_keys)
+
+
+        # Update the display
+    pygame.display.update()
